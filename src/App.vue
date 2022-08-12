5758178f4d7e5c726883be358fb947cea0332004
Artem Schirov <78075439+artemschirov@users.noreply.github.com>
2022-08-12 15:32:54 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">Hll Wrld!</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      api_key: 'd1cbf50ad630c263341b5810669be5a1',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
